b7ef610fb560fbcbb7c08e2b4e75750f357b29e9
Joao <froes544@gmail.com>
2026-05-27 22:45:23 -0300

    Init: projeto SPRING

---
+++ src/main/java/com/cefet/VVVSystem/VvvSystemApplication.java
@@ -0,0 +1,13 @@
+package com.cefet.VVVSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VvvSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VvvSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cefet/VVVSystem/VvvSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cefet.VVVSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VvvSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
