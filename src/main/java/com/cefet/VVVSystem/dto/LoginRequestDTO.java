3b468b47fb1072096bffefcb34c9622104094e40
Joao <froes544@gmail.com>
2026-06-13 00:11:45 -0300

    feat: adicionado swagger a aplicacao

--- src/main/java/com/cefet/VVVSystem/dto/LoginRequestDTO.java
+++ src/main/java/com/cefet/VVVSystem/dto/LoginRequestDTO.java
@@ -1,11 +1,14 @@
 package com.cefet.VVVSystem.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
 public record LoginRequestDTO(
     @NotBlank(message = "O nome de usuário não pode estar em branco")
+    @Schema(example = "admin")
     String username,
     
     @NotBlank(message = "A senha não pode estar em branco")
+    @Schema(example = "admin123")
     String password
 ) {}
