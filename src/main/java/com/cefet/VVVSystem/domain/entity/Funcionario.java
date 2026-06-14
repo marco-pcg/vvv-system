a4a257a908f1d6684f6a6b27f24ae903ae952997
André De Martin Guiot <demartinguiotandre@gmail.com>
2026-06-14 19:37:37 -0300

    fix: adiciona imports ausentes no Funcionario

--- src/main/java/com/cefet/VVVSystem/domain/entity/Funcionario.java
+++ src/main/java/com/cefet/VVVSystem/domain/entity/Funcionario.java
@@ -15,6 +15,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
